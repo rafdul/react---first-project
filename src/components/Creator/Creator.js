a26a602d3e317b8cb20f1cdf2d16aaf503b76c59
rafdul <rafalduluk@gmail.com>
2020-12-14 13:51:45 +0100

    Change text on the buttons

--- src/components/Creator/Creator.js
+++ src/components/Creator/Creator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Creator.scss';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
 
 class Creator extends React.Component {
   static propTypes = {
@@ -64,9 +65,9 @@ class Creator extends React.Component {
           onChange={this.handleChange}
         />
         <div className={styles.buttons + (this.state.visibleButtons ? ' ' + styles.buttonsShown : '')}>
-          <Button onClick={this.handleOK}>OK</Button>
+          <Button onClick={this.handleOK}>{settings.creator.buttonOK}</Button>
           {/* <Button onClick={this.handleCancel} variant='danger'>cancel</Button> */}
-          <Button onClick={this.handleCancelwithConfirm} variant='confirm'>cancel</Button>
+          <Button onClick={this.handleCancelwithConfirm} variant='confirm'>{settings.creator.buttonCancel}</Button>
         </div>
       </div>
     );
