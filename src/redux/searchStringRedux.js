d1bc0f6ea23b038bd3f80c5dfadedd618ff53098
rafdul <rafalduluk@gmail.com>
2020-12-17 13:04:52 +0100

    Change reducer for searching cards

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -6,14 +6,20 @@ export const countVisibleCards = ({cards, searchString}) => cards.filter(card =>
 export const countAllCards = ({cards}) => cards.length;
 
 // action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createAction_changeSearchString = payload => ({payload: payload, type: CHANGE});
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
