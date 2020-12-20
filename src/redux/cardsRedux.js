f04c49413263d2a508e114d0d54eab19c4f8aff9
rafdul <rafalduluk@gmail.com>
2020-12-20 13:38:43 +0100

    Move cards within column

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -9,15 +9,41 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const ADD_CARD = createActionName('ADD_CARD');
+export const MOVE_CARD = createActionName('MOVE_CARD');
 
 // action creators
 export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
+export const createAction_moveCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: MOVE_CARD });
 
 // reducer
 export default function reducer(state = [], action = {}) {
   switch (action.type) {
     case ADD_CARD:
       return [...state, action.payload];
+    
+    case MOVE_CARD: {
+      const {id, src, dest} = action.payload;
+      const targetCard = state.filter(card => card.id == id)[0];
+      const targetColumnCards = state.filter(card => card.columnId == dest.columnId).sort((a, b) => a.index - b.index);
+      console.log(targetColumnCards.map(card => `${card.index}, title: ${card.title}`));
+      
+      if(dest.columnId == src.columnId){
+        targetColumnCards.splice(src.index, 1);
+        targetColumnCards.splice(dest.index, 0, targetCard);
+
+        return state.map(card => {
+          const targetColumnIndex = targetColumnCards.indexOf(card);
+        
+          if(targetColumnIndex > -1 && card.index != targetColumnIndex){
+            return {...card, index: targetColumnIndex};
+          } else {
+            return card;
+          }
+        });
+      } else {
+        return state;
+      }
+    }
     default:
       return state;
   }
