c8e4a90ae1106832ecb32e2e504be82ac8c70475
rafdul <rafalduluk@gmail.com>
2021-01-02 21:12:34 +0100

    Change SearchResultsContainer to improve links in search results

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -6,10 +6,29 @@ import {createAction_changeSearchString} from '../../redux/searchStringRedux.js'
 
 const mapStateToProps = (state, props) => {
   const searchString = props.match.params.searchString;
-  
+  const cards = getCardsFromSearch(state, searchString);
+
+  state.cards.map(card => {
+    card.searchedCard = false;
+  });
+
+  cards.map(card => {
+    card.searchedCard = true;
+  });
+
+  const columns = state.columns.filter((columns) => {
+    return cards.map(card => {
+      if(columns.id === card.columnId) {
+        card.listId = columns.listId;
+        card.columnTitle = columns.title;
+      }
+      return columns.id === card.columnId;
+    });
+  });
 
   return {
-    cards: getCardsFromSearch(state, searchString),    
+    cards, 
+    columns, 
   };
 };
 
