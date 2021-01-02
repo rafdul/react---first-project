aced4afdf78c0389ef1d3f034c59dec5cb2a47be
rafdul <rafalduluk@gmail.com>
2021-01-02 16:37:01 +0100

    Implement changes for Search component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import styles from './Header.scss';
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon';
 import {settings, faqContents, infoContents, pageContents} from '../../data/dataStore';
+import Search from '../Search/SearchContainer';
 
 class Header extends React.Component {
 
@@ -21,6 +22,7 @@ class Header extends React.Component {
               <NavLink exact to='/faq' activeClassName='active'>{faqContents.categoryTitle}</NavLink>
             </nav>
           </div>
+          <Search />
         </Container>
       </header>
     );
