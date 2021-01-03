b1262a3c6bc3b8b873b0b9e28b99882ef09682d4
rafdul <rafalduluk@gmail.com>
2021-01-03 12:13:08 +0100

    Remove changeSearchString from SearchResultsContainer

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -1,8 +1,6 @@
 import {connect} from 'react-redux';
 import SearchResults from './SearchResults';
 import {getCardsFromSearch} from '../../redux/cardsRedux';
-import {createAction_changeSearchString} from '../../redux/searchStringRedux.js';
-
 
 const mapStateToProps = (state, props) => {
   const searchString = props.match.params.searchString;
@@ -32,8 +30,4 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
+export default connect(mapStateToProps)(SearchResults);
